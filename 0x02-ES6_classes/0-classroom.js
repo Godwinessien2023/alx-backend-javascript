77cb1f6b6fc9ba1f52f0de1be6da5fc8c5bb44ae
Godwin Essien <kinggodwin4u@yahoo.com>
2024-10-03 20:46:10 +0100

    task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
+// ClassRoom.js
+export default class ClassRoom {
+    constructor(maxStudentsSize) {
+      this._maxStudentsSize = maxStudentsSize;
+    }
+  }
