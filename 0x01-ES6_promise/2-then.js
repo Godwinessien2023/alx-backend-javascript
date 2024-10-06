bd8e2da31285c92c7810ccbed458d4d90e7aec3c
Godwin Essien <kinggodwin4u@yahoo.com>
2024-10-06 20:40:32 +0100

    promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function handleResponseFromAPI(promise) {
+    const body = { status: 200, body: 'success' };
+  
+    return promise
+      .then(() => body)
+      .catch(() => new Error())
+      .finally(() => {
+        console.log('Got a response from the API');
+      });
+  }
