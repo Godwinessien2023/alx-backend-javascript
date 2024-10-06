934e33eebf10e7bd13da1d119763549ff4fe7b2b
Godwin Essien <kinggodwin4u@yahoo.com>
2024-10-06 20:58:06 +0100

    promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -2,8 +2,10 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
 export default function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-      firstName,
-      lastName,
+    return new Promise((resolve, reject) => {
+      resolve({
+        firstName: firstName,
+        lastName: lastName
+      });
     });
   }
--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -2,5 +2,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
 export default function uploadPhoto(filename) {
-    return Promise.reject(new Error(`${filename} cannot be processed`));
+    return new Promise((resolve, reject) => {
+      reject(new Error(`${filename} cannot be processed`));
+    });
   }
