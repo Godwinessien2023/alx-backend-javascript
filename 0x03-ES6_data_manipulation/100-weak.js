f7ff5867fd0188fd9c5d1d97a52af7d8e9318aaa
Godwin Essien <kinggodwin4u@yahoo.com>
2024-10-09 03:26:18 +0100

    task 1

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 /* eslint-disable */
 /* eslint-disable no-unused-vars */
-export default function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-      return [];
-    }
-    return students.map(student => student.id);
-  }
